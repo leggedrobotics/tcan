677dfceaad0001de1abd08278b0cba71d391cfc7
Martin <martiwer@mavt.ethz.ch>
2017-06-15 08:38:08 +0200

    Add txqueuelen as optional argument.

--- tcan/scripts/canusb.sh
+++ tcan/scripts/canusb.sh
@@ -3,7 +3,7 @@
 do_start() {
     sudo slcand -o $3 -t hw -S 3000000 $1 $2
     sleep 0.2
-    sudo ip link set $2 txqueuelen 300
+    sudo ip link set $2 txqueuelen $4
     sudo ip link set $2 up
 }
 
@@ -14,7 +14,7 @@ do_stop() {
 
 case "$1" in
     start)
-        do_start $2 $3 $4
+        do_start $2 $3 $4 ${5:-1000}
     ;;
 
     stop)
@@ -23,13 +23,13 @@ case "$1" in
 
     restart)
         do_stop $3
-        do_start $2 $3 $4
+        do_start $2 $3 $4 ${5:-1000}
     ;;
 
     *)
-        echo "Usage: $0 {start|stop|restart} [<dev>] <name> [<can_rate>]"
+        echo "Usage: $0 {start|stop|restart} [<dev>] <name> [<can_rate>] [<txqueuelen>]"
         echo "    where dev is the file descriptor (e.g. /dev/ttyUSB0), name the name of"
-        echo "    the interface (e.g. can0), and can_rate one of the following:"
+        echo "    the interface (e.g. can0), can_rate one of the following:"
         echo "      -s8: 1Mbit"
         echo "      -s7: 800kbit"
         echo "      -s6: 500kbit"
@@ -38,5 +38,6 @@ case "$1" in
         echo "      -s3: 100kbit"
         echo "      -s2: 50kbit"
         echo "      -s1: 10kbit"
+        echo "    and txqueuelen the length of the transmit queue (default=1000)"
     ;;
 esac
