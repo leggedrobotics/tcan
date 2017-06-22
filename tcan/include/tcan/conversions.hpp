203982fedb1cf00a2d86bb1fd94b408c733cc1fc
Gabriel Hottiger <gabriel.hottiger@mavt.ethz.ch>
2017-06-22 16:03:31 +0200

    add write conversions

--- tcan/include/tcan/conversions.hpp
+++ tcan/include/tcan/conversions.hpp
@@ -21,4 +21,12 @@ Value readValue(const uint8_t* data, const uint16_t pos) {
   return value;
 }
 
+template <typename Value>
+void writeValue(uint8_t* data, const uint16_t pos, const Value value) {
+  const uint16_t len = sizeof(Value);
+  for (uint16_t i = 0; i < len; i++) {
+    data[pos+i] = static_cast<uint8_t>(value >> i*8);
+  }
+}
+
 }
