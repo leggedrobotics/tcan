9cafe91ca85051dc66c2b4f7d088ee95615012c0
yifan <jiang.uhrmacher@gmail.com>
2018-03-28 12:42:45 +0200

    fine tune waitForState timeout value

--- tcan_ethercat/include/tcan_ethercat/EtherCatBus.hpp
+++ tcan_ethercat/include/tcan_ethercat/EtherCatBus.hpp
@@ -548,12 +548,12 @@ class EtherCatBus : public tcan::Bus<EtherCatDatagrams> {
      */
     bool waitForState(const uint16_t state, const uint16_t slave = 0) {
         ecx_statecheck(&ecatContext_, slave, state,  EC_TIMEOUTSTATE * 2);
-        const unsigned int maxChecks = 500;
+        const unsigned int maxChecks = 1000;
         unsigned int check = 0;
         do {
             sendProcessData();
             receiveProcessData();
-            ecx_statecheck(&ecatContext_, slave, state,  2000);
+            ecx_statecheck(&ecatContext_, slave, state,  1000);
             check++;
         } while (check <= maxChecks && (ecatContext_.slavelist[slave].state != state));
 
