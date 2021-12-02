c0ee545a0d4124322cd08051dd0e882a52dba48c
Simon Kerscher <simon.kerscher@mavt.ethz.ch>
2021-12-02 13:42:20 +0100

    Create tcan_bridge package

---
+++ tcan_bridge/include/tcan_bridge/tcan_bridge.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace tcan_bridge
+{
+
+class TcanBridge
+{
+public:
+  TcanBridge();
+
+  virtual ~TcanBridge();
+};
+
+}  // namespace tcan_bridge
---
+++ tcan_bridge/src/tcan_bridge.cpp
@@ -0,0 +1,9 @@
+#include "tcan_bridge/tcan_bridge.hpp"
+
+namespace tcan_bridge {
+
+TcanBridge::TcanBridge() {}
+
+TcanBridge::~TcanBridge() {}
+
+}  // namespace tcan_bridge
