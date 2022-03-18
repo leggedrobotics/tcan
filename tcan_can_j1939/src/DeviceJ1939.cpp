468614a1c38af883d4fd0a036dd948e07aa99072
bcizmeci <burak.cizmeci@gmail.com>
2022-03-18 07:16:32 +0100

    Ignore wrong messages

--- tcan_can_j1939/src/DeviceJ1939.cpp
+++ tcan_can_j1939/src/DeviceJ1939.cpp
@@ -3,15 +3,21 @@
 #include "tcan_can/CanBus.hpp"
 #include "tcan_can_j1939/J1939CanMsg.hpp"
 
+#include <linux/can.h>
+
 namespace tcan_can_j1939 {
 
 bool DeviceJ1939::initDevice() {
-    return bus_->addCanMessage(tcan_can::CanFrameIdentifier(getNodeId(), 0xFF), this, &DeviceJ1939::parseMessage);
+    return bus_->addCanMessage(tcan_can::CanFrameIdentifier(CAN_EFF_FLAG | getNodeId(), 0x800000FFU), this, &DeviceJ1939::parseMessage);
 }
 
 bool DeviceJ1939::parseMessage(const tcan_can::CanMsg& msg) {
     const auto pgn = static_cast<const J1939CanMsg&>(msg).getParameterGroupNumber();
     std::cout << "Parsing message " << msg.getCobId() << '\n';
+    if (pgnMap_.find(pgn) == pgnMap_.end()) {
+        std::cout << "Got unknown message\n";
+        return true;
+    }
     return pgnMap_[pgn]->parse(msg);
 }
 
